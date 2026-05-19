13008ac95454bdbccbebaeda61b02fde6705195b
Patrick Callahan <patricktcallahan18@gmail.com>
2026-05-19 09:29:37 +0200

    Use a config file for project constants

---
+++ config.py
@@ -0,0 +1,9 @@
+
+TRANSLATABLE_COLUMNS = [
+    "Q6_oa_comment", "Q6_rdm_comment", "Q6_fair_comment", "Q6_code_comment", "Q6_material_comment", "Q6_prereg_comment", "Q6_rr_comment",
+    "Q7_oa_comment", "Q7_rdm_comment", "Q7_fair_comment", "Q7_code_comment", "Q7_material_comment", "Q7_prereg_comment", "Q7_rr_comment",
+    "Q8_other_1", "Q8_other_2", "Q8_other_3",
+    "Q9_other_1", "Q9_other_2", "Q9_other_3",
+    "Q10_other_1", "Q10_other_2", "Q10_other_3",
+    "Q11_other_1", "Q11_other_2", "Q11_other_3", "Q12_1", "Q12_2", "Q12_3", "Q12_4", "Q12_5"
+]
